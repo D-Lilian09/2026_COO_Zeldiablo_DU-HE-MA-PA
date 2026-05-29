2478ebd6bf644cdfd3d300398de231ed259f01e1
Lilian_D <lilian.dubicq4@etu.univ-lorraine.fr>
2026-05-29 10:56:56 +0200

    ajout partiel du moteur

---
+++ src/DessinZeldiablo.java
@@ -0,0 +1,12 @@
+import moteurJeu.DessinJeu;
+
+import java.awt.*;
+
+public class DessinZeldiablo implements DessinJeu {
+    JeuZeldiablo jeu = new JeuZeldiablo();
+
+    public void dessiner(bufferedImage im){
+        Graphics g = im.getGraphics();
+        g.drawRectangle
+    }
+}
---
+++ src/JeuZeldiablo.java
@@ -0,0 +1,16 @@
+public class JeuZeldiablo implements jeu{
+    int x=1, y=1;
+
+    public void evoluer (commande c){
+        if (c.gauche);
+        x--;
+        if (c.droite);
+        x++;
+        if (c.haut);
+        y++;
+        if (c.bas);
+        y--;
+
+    }
+
+}
