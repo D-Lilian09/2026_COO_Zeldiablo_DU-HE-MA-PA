eade2fdfd1602c2e95a811b29c10caaefbae6ba0
Theo Mathiot <theo.mathiot5@etu.univ-lorraine.fr>
2026-05-29 10:46:01 +0200

    ajout de la méthode getPosition() dans Personnage.java et retirer les erreurs

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,29 +1,18 @@
 
 public  class Personnage {
-    private double hp;
     private int x;
     private int y;
-    private double dgt;
 
-    public Personnage(double hp, int x, int y, double dgt){
-        this.hp = hp;
+    public Personnage(int x, int y){
         this.x = x;
         this.y = y;
-        this.dgt = dgt;
     }
 
-    public void retirerHp(double retirer_hp){
-        if(retirer_hp > 0){
-            this.hp -= retirer_hp;
-        }
+    public int[] getPosition(){
+        int[] position = new int[3];
+        position [1] = this.x;
+        position [2] = this.y;
+
+        return position;
     }
-
-    public int getX(){return this.x;}
-
-    public int getY(){return this.y;}
-
-    public double getHp(){return this.hp;}
-
-    public double getDgt(){return this.dgt;}
-
 }
