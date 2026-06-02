337ee5dbb6f0792b2cca8f69e3615e1e9bbb148f
Lilian_D <lilian.dubicq4@etu.univ-lorraine.fr>
2026-06-02 23:25:52 +0200

    ajout des déplacements allers retours des ennemies

--- src/demoMoteur/JeuZeldiablo.java
+++ src/demoMoteur/JeuZeldiablo.java
@@ -13,6 +13,8 @@ public class JeuZeldiablo implements Jeu {
     Personnage perso;
     Labyrinthe l;
     ArrayList<Ennemie> ennemies;
+    ArrayList<Ennemie> ennemiesH; // Ennemis horizontaux
+    ArrayList<Ennemie> ennemiesV; // Ennemis verticaux
 
 
 
@@ -24,12 +26,26 @@ public class JeuZeldiablo implements Jeu {
         }
         this.perso = new Personnage(8, 14); //spawn du perso
         this.ennemies = new ArrayList<Ennemie>();
+        this.ennemiesH = new ArrayList<Ennemie>();
+        this.ennemiesV = new ArrayList<Ennemie>();
 
-        //ennemis
+        // Ennemis horizontaux
         Ennemie e1 = new Ennemie(1, 1, 28, 1, 1, 1);
+        ennemiesH.add(e1);
         ennemies.add(e1);
-        Ennemie e2 = new Ennemie(11, 8, 16, 8, 11, 8);
+
+        Ennemie e2 = new Ennemie(11, 8, 19, 8, 11, 8);
+        ennemiesH.add(e2);
         ennemies.add(e2);
+
+        Ennemie e3 = new Ennemie(10, 14, 22, 14, 10, 14);
+        ennemiesH.add(e3);
+        ennemies.add(e3);
+
+        // Ennemis verticaux
+        Ennemie e4 = new Ennemie(4, 3, 4, 10, 4, 3);
+        ennemiesV.add(e4);
+        ennemies.add(e4);
     }
 
     public void evoluer (Commande c, Labyrinthe l){
@@ -42,13 +58,16 @@ public class JeuZeldiablo implements Jeu {
         if (c.bas)
             this.perso.seDeplacer("bas",this.l);
 
-        // Déplacement des ennemis
-        for (Ennemie e : this.ennemies) {
-            if (e.finPaterne()) {
-                e.reset();
-            } else {
-                e.seDeplacerH();
-            }
+        // Déplacement des ennemis horizontaux
+        for (Ennemie e : this.ennemiesH) {
+            e.finPaterne();
+            e.seDeplacerH();
+        }
+
+        // Déplacement des ennemis verticaux
+        for (Ennemie e : this.ennemiesV) {
+            e.finPaterne();
+            e.seDeplacerV();
         }
     }
 
--- src/zeldiablo/Ennemie.java
+++ src/zeldiablo/Ennemie.java
@@ -3,10 +3,11 @@ package zeldiablo;
 
 public class Ennemie extends Personnage {
 
-        private int xFin;
-        private int yFin;
-        private int xDeb;
-        private int yDeb;
+    private int xFin;
+    private int yFin;
+    private int xDeb;
+    private int yDeb;
+    private boolean allerRetour; // true = aller vers fin, false = retour vers deb
 
 
     public Ennemie(int x, int y, int xf, int yf, int xd, int yd) {
@@ -15,24 +16,41 @@ public class Ennemie extends Personnage {
         this.yFin = yf;
         this.xDeb = xd;
         this.yDeb = yd;
+        this.allerRetour = true; // commence par aller vers la fin
     }
 
     public void seDeplacerH() {
         int[] pos = getPosition();
-        setPosition(pos[0] + 1, pos[1]);
+        if (allerRetour) {
+            setPosition(pos[0] + 1, pos[1]);
+        } else {
+            setPosition(pos[0] - 1, pos[1]);
+        }
     }
 
     public void seDeplacerV() {
         int[] pos = getPosition();
-        setPosition(pos[0], pos[1] + 1);
+        if (allerRetour) {
+            setPosition(pos[0], pos[1] + 1);
+        } else {
+            setPosition(pos[0], pos[1] - 1);
+        }
     }
 
     public boolean finPaterne() {
         int[] pos = getPosition();
-        return (pos[0] == xFin && pos[1] == yFin);
+        if (allerRetour && pos[0] == xFin && pos[1] == yFin) {
+            allerRetour = false; // inverse la direction
+            return true;
+        } else if (!allerRetour && pos[0] == xDeb && pos[1] == yDeb) {
+            allerRetour = true; // inverse la direction
+            return true;
+        }
+        return false;
     }
 
     public void reset() {
         setPosition(xDeb, yDeb);
+        allerRetour = true;
     }
 }
